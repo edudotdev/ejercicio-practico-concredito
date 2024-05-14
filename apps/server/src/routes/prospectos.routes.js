4388d39cff49b7d56533e2df933e916b5d7ed8c2
edudotdev <eduardo.er626@gmail.com>
2024-05-14 00:05:19 -0700

    feat: middleware to read files and data form

--- apps/server/src/routes/prospectos.routes.js
+++ apps/server/src/routes/prospectos.routes.js
@@ -1,11 +1,23 @@
 import { Router } from "express";
 import { ProspectoController } from '../controllers/prospectos.controller.js'
- 
+import multer from 'multer'
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null,'uploads')
+  },
+  filename: function (req, file, cb) {
+    const noSpaces = file.originalname.trim()
+    cb(null, `${Date.now()}-${noSpaces}`)
+  }
+})
+
+const upload = multer({ storage })
 const router = Router()
 
 router.get('/prospectos', ProspectoController.getProspectos)
 router.get('/prospectos/:id', ProspectoController.getProspectoById)
-router.post('/prospectos', ProspectoController.createProspecto)
+router.post('/prospectos', upload.any(), ProspectoController.createProspecto)
 router.put('/prospectos/:id', ProspectoController.updateProspectoStatus)
 router.delete('/prospectos/:id', ProspectoController.deleteProspectoById)
 
