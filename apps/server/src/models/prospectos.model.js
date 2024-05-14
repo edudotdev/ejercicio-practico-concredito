da81cc37006a6fc79793002960cbd5e588133b71
edudotdev <eduardo.er626@gmail.com>
2024-05-14 00:30:28 -0700

    feat: model for create prospecto

--- apps/server/src/models/prospectos.model.js
+++ apps/server/src/models/prospectos.model.js
@@ -9,23 +9,35 @@ export class ProspectoModel {
   }
 
   static async getProspectoById(id) {
-    const [rows] = await pool.query('SELECT * FROM prospecto WHERE id = ?', [id])
+    const [rows] = await pool.query('CAll getProspectoAndDocumentsById(?)', [id])
     if (rows.length <= 0) return res.status(404).json({
       message: "Prospecto not found"
     })
 
     return [rows]
   }
+
   //create
-  static async createProspecto(prospectoData) {
+  static async createProspecto(prospectoData, prospectoFiles) {
     const { name, first_lastName, second_lastName, street, house_number, 
-      street2, postal_code, phone, rfc, status, observations
+      street2, postal_code, phone, rfc
     } = prospectoData
-    const [result] = await pool.query("INSERT INTO prospecto (name, first_lastName, second_lastName, street, house_number, street2, postal_code, phone, rfc, status, observations) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+ 
+    const [result] = await pool.query('INSERT INTO prospecto (name, first_lastName, second_lastName, street, house_number, street2, postal_code, phone, rfc, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
     [ name, first_lastName, second_lastName, street, house_number, 
-      street2, postal_code, phone, rfc, status, observations
+      street2, postal_code, phone, rfc, 'Sent'
     ])
-    
+
+    if(result.affectedRows = 1) {
+      const prospectoId = result.insertId
+
+      for (const prospectoFile of prospectoFiles) {
+        const { filename, path, size, mimetype} = prospectoFile
+        const [resultDocuments] = await pool.query('INSERT INTO document (prospecto_id, name, path, size, mimetype) VALUES (?, ?, ?, ?, ?)',
+        [prospectoId, filename, path, size, mimetype])
+      }
+    }
+
     return [result]
   }
 
@@ -33,14 +45,14 @@ export class ProspectoModel {
   static async updateProspectoStatus(id, statusData) {
     const { status, observations } = statusData
     const [result] = await pool.query('UPDATE prospecto SET status = ?, observations = ? WHERE id = ?', [status, observations, id])
-    
-    return result
+
+    return result 
   }
 
   //delete
   static async deleteProspectoById(id) {
     const [result] = await pool.query('DELETE FROM prospecto WHERE id = ?', [id])
-    
+  
     return [result]
   }
 }
